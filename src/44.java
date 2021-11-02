241094b90cdc2064f7e679667d6f8f65ddff9407
Gal <galsened@gmail.com>
2021-11-02 10:21:46 +0200

    ok

--- src/44.java
+++ src/44.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.*;
+//import org.testng.annotations.*;
 
 public class App {
 	public static void main(String[] args) throws Exception {
