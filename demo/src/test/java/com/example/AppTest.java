63adb09990a10e0b51b0688c7d1f126b5a097eda
Gal <galsened@gmail.com>
2021-11-02 17:03:52 +0200

    ok

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -1,20 +1,42 @@
-package com.example;
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Driver;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.annotations.*;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.chromium.ChromiumDriverCommandExecutor;
+import org.openqa.selenium.html5.LocationContext;
+import org.openqa.selenium.html5.WebStorage;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        System.out.println("-----This is the beginning of our test !!!-----");
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Gal\\Desktop\\Oxs_Testing\\New_AutomtionOXS\\New_AutomationOXS\\chromedriver.exe");
+
+        driver = new ChromeDriver();
+
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void Login() {
+
+        driver.get("https://stg.oxs.co.il");
+        driver.manage().window().maximize();
     }
 }
